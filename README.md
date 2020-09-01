e6a6ded025aa55014e8b7be70412d7c7ff517a27
juliusmlambo <jjdelisamlambo@gmail.com>
2020-09-01 14:50:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-2fbu2l
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-2fbu2l)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+   employees = [{name:"Julius", surname:"Mlambo", position:"CEO", age:27},
+                   {name:"Peter", surname:"Brown", position:"Executive Director", age:32},
+                   {name:"Angel", surname:"Zulu", position:"Managing Director", age:22},
+                   {name:"Stanly", surname:"Scott", position:"Chief Financial Office", age:28},
+                   {name:"Amanda", surname:"Solo", position:"Online Manager", age:29},
+                   {name:"Chris", surname:"Malana", position:"Chief Technology officer", age:33},
+                   {name:"Noma", surname:"Mzobe", position:"Developer", age:22},
+                   {name:"Arthur", surname:"Mazibuko", position:"Front-end developer", age:23},
+                   {name:"Anathi", surname:"Xulu", position:"Software Developer", age:21},
+                   {name:"Amo", surname:"Zwane", position:"Intern", age:19},
+  ];
+}
